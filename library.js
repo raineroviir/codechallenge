fffa4adf2de1f182c958dae5e717c82e441fc9fb
Rainer Oviir <roviir@gmail.com>
2015-04-17 17:11:39 -0700

    moved add/removes to prototypes and re-added Book network

--- library.js
+++ library.js
@@ -1,30 +1,35 @@
 // to make this library modular, use module exports, EX: module.exports.Shelf = //shelf constructor code
 
-function Library()
+function Library() {}
+
+Library.prototype.addShelf = function (shelfName)
 {
-  this.addShelf = function (shelfName)
-  {
-    return this[shelfName] = new Shelf();
-  }
-  this.removeShelf = function (shelfName)
-  {
-    delete this[shelfName];
-    return this
-  }
+  return this[shelfName] = new Shelf();
 }
 
-function Shelf()
+Library.prototype.removeShelf = function (shelfName)
 {
-  this.remove = function(bookName)
-  {
-    delete this[bookName]
-    return this
-  }
-  this.add = function(bookName, author)
-  {
-    this[bookName] = author;
-    return this
-  }
+  delete this[shelfName];
+  return this
+}
+
+function Shelf() {}
+
+Shelf.prototype.add = function(bookName, author, pages)
+{
+  this[bookName] = new Book(author, pages);
+  return this
+}
+
+Shelf.prototype.remove = function(bookName)
+{
+  delete this[bookName];
+  return this
+}
+
+function Book(author, pages) {
+  this.author = author
+  this.pages = pages
 }
 
 //some data to populate the library:
@@ -32,11 +37,11 @@ function Shelf()
 var library = new Library();
 
 library.addShelf("Shelf1")
-.add("my america","Stephen Colbert")
-.add("journey into the unknown", "JRR Tolkien")
-.remove("my america")
-.remove("journey into the unknown")
+.add("My America","Stephen Colbert", 500)
+.add("Journey into the unknown", "JRR Tolkien", 999)
+.remove("My America")
+.remove("Journey into the unknown")
 
-library.addShelf("Shelf99").add("awesome", "that one author")
+library.addShelf("Shelf99").add("Awesome", "That one author", 7)
 library.removeShelf("Shelf1")
 
